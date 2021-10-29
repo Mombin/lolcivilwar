e36d665979a2f72a496b2230fced8b9d0f3c6615
권형민 <hil00137@gmail.com>
2021-10-30 00:39:51 +0900

    LOLCW-8 #time 15m login 페이지 작업을 위한WebUserEntity, BaseTimeEntity 이관

---
+++ src/main/java/kr/co/mcedu/user/entity/WebUserEntity.java
@@ -0,0 +1,25 @@
+package kr.co.mcedu.user.entity;
+
+import kr.co.mcedu.common.entity.BaseTimeEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "web_user")
+@Table(name = "web_user", schema = "lol")
+@SequenceGenerator(sequenceName = "web_user_seq", initialValue = 1, allocationSize = 1, name = "web_user_seq_generator", schema = "lol")
+public class WebUserEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(generator = "web_user_seq_generator", strategy = GenerationType.SEQUENCE)
+    private Long userSeq;
+    private String userId;
+    private String password;
+    private String authority;
+    private String email;
+    private Boolean confirm = false;
+}

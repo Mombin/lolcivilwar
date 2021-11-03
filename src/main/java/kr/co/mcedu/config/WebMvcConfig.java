f6007b8377b96298cde865e9c740d0e99064eb8c
hil00137 <hil00137@gmail.com>
2021-11-03 21:51:54 +0900

    LOLCW-19 #time 20m api url을 기록하기 위한 핸들러 적용

--- src/main/java/kr/co/mcedu/config/WebMvcConfig.java
+++ src/main/java/kr/co/mcedu/config/WebMvcConfig.java
@@ -1,14 +1,21 @@
 package kr.co.mcedu.config;
 
+import kr.co.mcedu.config.web.RequestHandler;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addInterceptors(final InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(requestHandler());
+    }
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         // url : /static/** -> classpath:/static/**
@@ -31,4 +38,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public SpringSecurityDialect springSecurityDialect()  {
         return new SpringSecurityDialect();
     }
+
+    @Bean
+    public RequestHandler requestHandler() { return new RequestHandler(); }
 }

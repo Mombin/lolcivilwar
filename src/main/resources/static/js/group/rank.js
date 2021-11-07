f7b31f919dccd73c138725e4201447d955c6a9cf
권형민 <hil00137@gmail.com>
2021-11-07 11:58:01 +0900

    LOLCW-21 #time 30m 라인별승률 페이지 이관

---
+++ src/main/resources/static/js/group/rank.js
@@ -0,0 +1,46 @@
+let currentGroup = {};
+
+function setList(list) {
+    const $list = $("#list")
+    $list.empty();
+
+    let number = 1;
+    $.each(list, function (index, item) {
+        let rank;
+        if (item.total === 0) {
+            return;
+        } else if (item.rate > 55) {
+            rank = 'table-danger';
+        } else if (item.rate < 45) {
+            rank = 'table-secondary';
+        }
+        let $tr = $('<tr>')
+            .addClass(rank)
+            .append($('<th>').attr('scope', 'row').html(number))
+            .append($('<td>').html(item.nickname))
+            .append($('<td>').html(item.summonerName))
+            .append($('<td>').html(item.total))
+            .append($('<td>').html(item.win))
+            .append($('<td>').html(item.rate))
+            .append($('<td>').html(item.lastDate));
+
+        $list.append($tr);
+        number++;
+    });
+}
+
+// 그룹 선택 이벤트
+function changeGroupSelect() {
+    currentGroup = $(this).find('option:selected').data('group');
+    $('#position .nav-link.active').trigger('click');
+}
+
+function groupChangeFunction(groupList) {
+    for (let i = 0; i < groupList.length; i++) {
+        if (groupList[i].auth === GROUP_AUTH.OWNER) {
+            $groupSelector.val(groupList[i].groupSeq)
+            $groupSelector.trigger('change');
+            return;
+        }
+    }
+}
---
+++ src/main/resources/static/js/group/rank_event.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    bindObject();
+    bindEvent();
+    init();
+});
+
+// 객체 바인딩
+function bindObject() {
+    $groupSelector = $("#groupSelector")
+}
+
+// 이벤트 바인딩
+function bindEvent() {
+    $("#position .nav-link").on('click', function () {
+        $("#position .nav-link.active").removeClass('active');
+        $(this).addClass('active');
+        let position = $(this).data('position');
+        let list = [];
+        $.each(currentGroup.customUser, function (index, item) {
+            let date = (item.lastDate || []).slice();
+            let lastDate;
+            if (date) {
+                date[1] = date[1] - 1;
+                lastDate = moment(date.slice(0, 6));
+                if (moment().diff(lastDate, 'day') > 10) {
+                    return;
+                }
+            } else {
+                return;
+            }
+            let total = 0
+                , win = 0;
+            if (position === 'total') {
+                $.each(item.positionWinRate, function (key, value) {
+                    total += value.first || 0;
+                    win += value.second || 0;
+                });
+            } else {
+                let positionWinRate = item.positionWinRate[position.toUpperCase()] || {};
+                total = positionWinRate.first || 0;
+                win = positionWinRate.second || 0;
+            }
+            let rate = ((win / (total === 0 ? 1 : total)) * 100).toFixed(1);
+            if (isNaN(rate)) {
+                rate = 0;
+            }
+
+            list.push({
+                nickname: item.nickname,
+                summonerName: item.summonerName,
+                total: total,
+                win: win,
+                rate: rate,
+                lastDate: lastDate.format('YYYY-MM-DD')
+            })
+        });
+        list.sort(function (a, b) {
+            let check = b.rate - a.rate;
+            if (check !== 0) {
+                return check;
+            }
+            return b.total - a.total;
+        });
+        setList(list);
+    });
+
+    $groupSelector.on('change', changeGroupSelect);
+}
+
+function init() {
+    callMyGroup('/api/group/my', groupChangeFunction);
+}

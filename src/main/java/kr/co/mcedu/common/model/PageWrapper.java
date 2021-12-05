ea2ccd4e8b5d680aed6370f2a1705a52fce580f2
권형민 <hil00137@gmail.com>
2021-12-06 01:17:11 +0900

    LOLCW-55 #time 15m 전체 페이지 수 추가, 현재 페이지 추가

--- src/main/java/kr/co/mcedu/common/model/PageWrapper.java
+++ src/main/java/kr/co/mcedu/common/model/PageWrapper.java
@@ -13,21 +13,27 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 public class PageWrapper<T> {
-    private List<T> data;
+    private List<T> list;
     private Long total;
     private Long pageSize;
-    public PageWrapper(Collection<T> collection, long total, long pageSize) {
-        this.data = new ArrayList<>(collection);
+    private Long offset;
+    private Long pageNum;
+    private Integer totalPage;
+    public PageWrapper(Collection<T> collection, long total, long pageSize, final long offset) {
+        this.list = new ArrayList<>(collection);
         this.total = total;
         this.pageSize = pageSize;
+        this.offset = offset;
+        this.pageNum = offset / pageSize;
+        this.totalPage = (int) Math.ceil((double) total / (double) pageSize);
     }
 
     public <R> PageWrapper<R> change(Function<T, R> changer) {
-        List<R> collect = this.data.stream().map(changer).collect(Collectors.toList());
-        return new PageWrapper<>(collect, total, pageSize);
+        List<R> collect = this.list.stream().map(changer).collect(Collectors.toList());
+        return new PageWrapper<>(collect, total, pageSize, offset);
     }
 
     public static <T> PageWrapper<T> of(final QueryResults<T> queryResults) {
-        return new PageWrapper<>(queryResults.getResults(), queryResults.getTotal(), queryResults.getLimit());
+        return new PageWrapper<>(queryResults.getResults(), queryResults.getTotal(), queryResults.getLimit(), queryResults.getOffset());
     }
 }

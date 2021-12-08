cd777638c1e617791a9bd5d3329b660ceab24edd
권형민 <hil00137@gmail.com>
2021-12-09 00:49:29 +0900

    LOLCW-56 #time 40m 초대알림 구현

--- src/main/java/kr/co/mcedu/user/model/UserAlarmMessage.java
+++ src/main/java/kr/co/mcedu/user/model/UserAlarmMessage.java
@@ -1,5 +1,6 @@
 package kr.co.mcedu.user.model;
 
+import kr.co.mcedu.user.entity.GroupInviteEntity;
 import kr.co.mcedu.user.entity.UserAlarmEntity;
 import kr.co.mcedu.utils.TimeUtils;
 import lombok.Getter;
@@ -14,9 +15,15 @@ public class UserAlarmMessage {
     private final Long alarmSeq;
     private final String message;
     private final String viewTime;
+    private final UserAlarmType alarmType;
+    private Long inviteSeq;
     public UserAlarmMessage(UserAlarmEntity userAlarmEntity) {
         this.alarmSeq = Optional.ofNullable(userAlarmEntity.getAlarmSeq()).orElse(0L);
         this.message = userAlarmEntity.getMessage();
         this.viewTime = TimeUtils.convertViewTime(userAlarmEntity.getCreatedDate());
+        this.alarmType = userAlarmEntity.getAlarmType();
+        if (this.alarmType == UserAlarmType.INVITE) {
+            inviteSeq = Optional.ofNullable(userAlarmEntity.getGroupInviteEntity()).map(GroupInviteEntity::getGroupInviteSeq).orElse(null);
+        }
     }
 }
--- src/main/resources/static/js/user/message.js
+++ src/main/resources/static/js/user/message.js
@@ -1,4 +1,7 @@
 let $unreadMessage;
+const ALARM_TYPE = {
+  MESSAGE: 'MESSAGE'
+}
 
 // 읽지 않은 알림 불러오기
 function getUnreadMessage() {
@@ -7,6 +10,7 @@ function getUnreadMessage() {
             toast.error("메시지를 불러오는데 에러가 발생하였습니다.\n관리자에게 문의부탁드립니다.");
             return;
         }
+        $unreadMessage.empty();
         if (res.data.length === 0) {
             $unreadMessage.append(
                 $('<div>').addClass('card text-center').append(
@@ -17,23 +21,60 @@ function getUnreadMessage() {
             return;
         }
         $.each(res.data, function (index, item) {
-           $unreadMessage.append(
-               $('<div>').addClass('card')
-                   .append(
-                       $('<div>').addClass('card-body')
-                           .append($('<p>').addClass('card-text').html(item.message))
-                           .append(
-                               $('<a>').attr('href', '#')
-                                   .addClass('btn btn-primary')
-                                   .data('alarmSeq', item.alarmSeq)
-                                   .on('click', readMessage)
-                                   .html('확인')
-                           )
-                   )
-                   .append(
-                       $('<div>').addClass('card-footer text-muted').html(item.viewTime)
-                   )
-           )
+          let alarm = '';
+          if (item.alarmType === ALARM_TYPE.MESSAGE) {
+            alarm = typeMessage(item);
+          } else {
+            alarm = typeInvite(item);
+          }
+          $unreadMessage.append(alarm)
         });
     });
+}
+
+function typeMessage(item) {
+  return $('<div>').addClass('card')
+    .append(
+      $('<div>').addClass('card-body')
+        .append($('<p>').addClass('card-text').html(item.message))
+        .append(
+          $('<a>').attr('href', '#')
+            .addClass('btn btn-primary')
+            .data('alarmSeq', item.alarmSeq)
+            .on('click', readMessage)
+            .html('확인')
+        )
+    )
+    .append(
+      $('<div>').addClass('card-footer text-muted').html(item.viewTime)
+    )
+}
+
+function typeInvite(item) {
+  return $('<div>').addClass('card')
+    .append(
+      $('<div>').addClass('card-body')
+        .append($('<p>').addClass('card-text').html(item.message))
+        .append(
+          $('<a>').attr('href', '#')
+            .addClass('btn btn-primary mr10')
+            .data('alarmSeq', item.alarmSeq)
+            .data('inviteSeq', item.inviteSeq)
+            .data('result', 'Y')
+            .on('click', replyInvite)
+            .html('승인')
+        )
+        .append(
+          $('<a>').attr('href', '#')
+            .addClass('btn btn-danger')
+            .data('alarmSeq', item.alarmSeq)
+            .data('inviteSeq', item.inviteSeq)
+            .data('result', 'N')
+            .on('click', replyInvite)
+            .html('취소')
+        )
+    )
+    .append(
+      $('<div>').addClass('card-footer text-muted').html(item.viewTime)
+    )
 }

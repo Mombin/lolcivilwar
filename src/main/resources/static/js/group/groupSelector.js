b75924d7e637270bdf8958c62814de6343f96edb
hil00137 <hil00137@gmail.com>
2021-12-12 01:23:59 +0900

    LOLCW-63 #time 20m 배포 및 확인할 그룹이 없을경우에도 groupSelector 노출, 선택된 그룹에 대한 조건 확인

--- src/main/resources/static/js/group/groupSelector.js
+++ src/main/resources/static/js/group/groupSelector.js
@@ -1,19 +1,21 @@
-let $groupSelector, $seasonSelector, currentGroup = {}
+let $groupSelector, $seasonSelector, currentGroup = {
+    groupSeq: 0
+}
 
 function callMyGroup(url, changeFn, changeFnParam) {
     $groupSelector = $("#groupSelector");
     let groupList = [];
     common_ajax.call(url, 'POST', false, {}, function (res) {
         groupList = res.data;
+        $groupSelector.empty();
+        $groupSelector.append($('<option>').val(0).attr('selected', true).attr('disabled', true).html('그룹을 선택해주세요'));
+        $groupSelector.show();
         if (groupList.length === 0) {
             toast.warning("그룹에 속해 있지 않습니다.")
             return;
         }
-        $groupSelector.empty();
-        $groupSelector.append($('<option>').val(0).attr('selected', true).attr('disabled', true).html('그룹을 선택해주세요'));
         groupList.forEach((item, index) => {
             $groupSelector.append($('<option>').val(item.groupSeq).html(item.groupName).data('group', item));
-            $groupSelector.show();
         });
 
     });
@@ -41,4 +43,12 @@ function getMatchAttendees(seasonSeq) {
             }
         })
     });
+}
+
+function checkGroupSeq() {
+    if (currentGroup.groupSeq === 0) {
+        toast.error("선택한 그룹이 없습니다.")
+        return false;
+    }
+    return true;
 }

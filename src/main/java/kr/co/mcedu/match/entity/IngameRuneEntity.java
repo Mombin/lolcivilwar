dc2c90a752ce4e278fc402b2dde9527ddfc1bf65
parrot <buj6842@naver.com>
2021-12-20 22:06:31 +0900

    LOLCW-73 #time 1h entity 작성

---
+++ src/main/java/kr/co/mcedu/match/entity/IngameRuneEntity.java
@@ -0,0 +1,31 @@
+package kr.co.mcedu.match.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "ingame_rune")
+@Table(name = "ingame_rune", schema = "lol")
+@SequenceGenerator(sequenceName = "ingame_rune_seq", initialValue = 1, allocationSize = 1, name = "ingame_rune_seq", schema = "lol")
+public class IngameRuneEntity {
+
+    @Id
+    @Column(name = "rune_seq")
+    @GeneratedValue(generator = "ingame_rune_seq", strategy = GenerationType.SEQUENCE)
+    private Long runeSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attendees_seq",referencedColumnName ="attendees_seq")
+    private MatchAttendeesEntity attendeesSeq;
+
+    @Column(name = "rune_type")
+    private String runeType;
+
+    @Column(name = "rune_id")
+    private Long rundId;
+}
---
+++ src/main/java/kr/co/mcedu/match/entity/MatchBanChamp.java
@@ -0,0 +1,31 @@
+package kr.co.mcedu.match.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "match_ban_champ")
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "match_ban_champ")
+@SequenceGenerator(sequenceName = "match_ban_seq", initialValue = 1, allocationSize = 1, name = "match_ban_seq", schema = "lol")
+public class MatchBanChamp {
+
+    @Id
+    @Column(name = "match_ban_seq")
+    @GeneratedValue(generator = "match_ban_seq", strategy = GenerationType.SEQUENCE)
+    private Long matchBanSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "match_seq" , referencedColumnName = "match_seq")
+    private CustomMatchEntity customMatch;
+
+    @Column(name = "ban_order")
+    private String banOrder;
+
+    @Column(name = "ban_champ_id")
+    private Long banChampId;
+}
